84e2eafd467ef2feb2e0d8c4050f9daccf012cf1
Emaleigh Adams <emaleigh.a304@stu.nebo.edu>
2019-01-28 10:10:00 -0700

    Properly start the story: Dungeon

--- Adventure_Game/game.js
+++ Adventure_Game/game.js
@@ -11,10 +11,30 @@ Multiline comment
 
 // prompt("What type of pokemon do you like?");
 
-var pc = prompt("What is your name?");
+Game();
 
-document.write("Hello ");
-
-document.write(pc);
-
-document.write(", Welcome to the universe of TACOS!");
+function Game(){
+    
+    alert("Land of Brave Princesses and Cowardly Knights!");
+    
+    var playerName = prompt("What is your name?");
+    
+    alert("Welcome to the land of Narkia "+ playerName);
+    
+    Castle_Dungeon();
+    
+    function Castle_Dungeon(){
+        var dungeon = prompt("You wake up in a pitch black place... The ground is wet and you can a hear a faint dripping sound from somewhere nearby. After getting up and wandering around for a bit you discover that you are in an underground, circular prison cell. There is a door on the left side of the space, that is the only exit. Probably wasnt the best idea to call the queen ugly and fat in front of Palace guards at that Tavern. \n -Try the door \n -Lay down and wait for a guard or something \n -Search floor to hopefully find something useful");
+    
+        if(dungeon == "try the door" || dungeon == "door"){
+            
+            var dungeonDoor = confirm("You check the door... It's locked. \n Go back?");
+            
+            Castle_Dungeon();
+        }
+        else if(dungeon == "lay down and wait"|| dungeon || "wait"){
+            
+            var dungeonWait = confirm("")
+        }
+    }
+}
