32d607dcfa6071ab364a4a0e0f502223f501cff2
Emaleigh Adams <emaleigh.a304@stu.nebo.edu>
2019-03-01 10:01:29 -0700

    COMPLETION

--- Adventure_Game/game.js
+++ Adventure_Game/game.js
@@ -23,6 +23,13 @@ Multiline comment
 }
 */
 
+/*
+for (i =1; i <= buyArrows; i++){
+    inventory.arrows++;
+    inventory.coins -= 10;
+}
+*/
+
 
 //Javascript Object for an inventory
     var inventory = {
@@ -37,9 +44,18 @@ Multiline comment
     
 //quit game repeat function
 var quitGame = function (){
-        alert("Game Over!");
+        alert("Game Over, "+playerName+"!");
     quitGame();
     }
+
+//Congradulations!!!
+var winGame = function (){
+        alert("Congradulations!");
+    winGame();
+}
+
+//Float
+var you = 90.9;
 
 Game();
 
@@ -55,7 +71,7 @@ function Game(){
         playerName = prompt("What do you want your name to be?");
     }
     
-    alert("Welcome to the land of Narkia, "+ playerName +".");
+    alert("Welcome to the land of Narkia, "+ playerName +".  You have a "+String(you)+" percent chance of dying in this game.... good luck.");
     
     
     
@@ -80,7 +96,7 @@ function Game(){
             Castle_Dungeon();
         }
 //dungeon search
-        else if(dungeon == "search floor"|| dungeon == "search"){
+        else if(dungeon == "search floor"|| dungeon == "search" || dungeon == "search floor to hopefully find something useful"){
             
             var dungeonSearch = confirm("You Vigourously search the dungeon floor... You found a key!! You try the key on the door and it unlocks. \n Do you continue?");
             
@@ -222,21 +238,45 @@ function Game(){
             Opening_again();
         }
     }
-//Staircase out (INCOMPLETE)
+//Staircase out
     Staircase();
     
     function Staircase(){
         var stairCase = prompt("You went up the staircase and made your way out.  Congradulations! You've made it out into the open fresh air without being eaten by a scary monster.... but now what?  You've escaped but now you cant remember your original reason for coming to this country.  \n -Sit and ponder \n -Go North \n -Go South").toLowerCase();
         
-        if(stairCase == "sit and ponder" || "sit" || "ponder"){
+        if(stairCase == "sit and ponder" || stairCase == "sit" ||stairCase == "ponder"){
             alert("...")
             alert("well that was pointless.");
             Staircase();
         }
-        else if(stairCase == "go north" || "north"){
+        else if(stairCase == "go north" || stairCase == "north"){
             alert("You make your way north and to your surprise you run into a city ruled by angels.  It is beautiful and bright and positively covered in the color white.  The angels spot you approaching and come to investigate.");
             
+            if(inventory.BadLuck > 0){
+                alert("The Angels gaze at you for a moment. Seeing that you have evil badluck, they give you a pitying glare and the next second you find yourself impaled with a spear. You died.");
+                quitGame();
+            }
+            else{
+                alert("The Angels deem that you are an amazingly good person, pick you up and take you to the great heaven above!");
+                winGame();
+            }
         }
+        else if(stairCase == "go south" || stairCase == "south"){
+            alert("You make your way south and to a very unpleasant thought, you discover a city ruled by demons!  The city is covered in an intimidating black and red colors.  The demons spot you approaching and come to investigate.");
+            
+            if(inventory.BadLuck > 0){
+                alert("The demons see you and observe that you are a horrible person, a thief!  They celebrate your existence and take you to the entrance to hell so that you may party with them for eternity!");
+                Entrance_hell();
+            }
+            else{
+                alert("The demons sense that you are a good person and they dont like it!  They pick you up with their strong arms and crucify you. You died.");
+                quitGame();
+            }
+        }
+         else{
+            alert("I dont understand what "+stairCase+" means.");
+            Staircase();
+            }
     }
 //Opening Again
     Opening_again();
@@ -248,12 +288,12 @@ function Game(){
             
             Openingtakeally();
         }
-        else if(openingAgain == "move back to staircase" || "staircase" || "go back"){
+        else if(openingAgain == "move back to staircase" || openingAgain == "staircase" || openingAgain == "go back"){
         
             Staircase();
     }
         
-        else if(openingAgain == "just lay down and die" || "lay down and die" || "just die" || "die"){
+        else if(openingAgain == "just lay down and die" || openingAgain == "lay down and die" || openingAgain == "just die" || openingAgain == "die"){
             alert("Well I guess if its too hard then...");
             quitGame();
         }
@@ -269,7 +309,7 @@ function Game(){
     function Opening_takeall(){
         var openingTakeall = prompt("You have gone back to the center of the room, would you like to turn on the lantern and continue forward? Or would you like to risk the dark and hope certain evil creatures dont come to kill you? \n - Light lantern and continue \n - risk the dark").toLowerCase();
         
-        if(openingTakeall == "light lantern and continue" || "light lantern" || "lantern"){
+        if(openingTakeall == "light lantern and continue" || openingTakeall == "light lantern" || openingTakeall == "lantern"){
             
             var takeallLantern = confirm("You light the lantern and make your way down a narrow corridor that you didnt realize was there till then. Congrats. You avoided being eaten by the infamous "+enemynames[1]+".  While traveling down the corridor you come across a staircase. would you like to go up?");
             
@@ -281,13 +321,35 @@ function Game(){
                 Staircase();
             }
         }
-        else if(openingTakeall == "risk the dark" || "the dark" || "dark"){
+        else if(openingTakeall == "risk the dark" || openingTakeall == "the dark" || openingTakeall == "dark"){
             alert("You decide to brave the dark for some weird reason and end up being eaten by "+enemynames[2]+".  You have died.");
             quitGame();
         }
         else{
             alert("I dont understand "+takeallLantern+".");
             Opening_takeall();
+        }
+    }
+    
+    //The Entrance to Hell with bugs bunny
+    Entrance_hell();
+    
+    function Entrance_hell(){
+        var entranceHell = prompt("At the entrance you meet a strange create.... its "+enemynames[2]+"!  He is very huge, very fat and very hungry.  How will you get him to move out of the way so that you can get your happy/unhappy ending? \n - Push him \n - Feed him your lunch \n - Give up").toLowerCase();
+        
+        if(entranceHell == "push him" || entranceHell == "push"){
+            alert("You and the demons try pushing him but he is just toooooooo fat. Luckily "+enemynames[2]+" doesnt seem to get angry about it.");
+            Entrance_hell();
+        }
+        
+        else if(entranceHell == "feed him your lunch" || entranceHell == "feed him" || entranceHell == "lunch" || entranceHell == "feed him lunch"){
+            alert("You lead him away from the door with the lure of your delicious lunch *sad you wont be able to eat it* He leaves the doorway.  You give him your lunch.  You enter the existence called hell where the demons love you and party for eternity.");
+            winGame();
+        }
+        
+        else if(entranceHell == "give up"){
+            alert("You suck");
+            quitGame();
         }
     }
 }
